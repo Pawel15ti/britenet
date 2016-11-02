1205c7c8718f875479ecab64d124206b2adfa7e7
Pawelozdoba07 <Pawelozdoba07@gmail.com>
2016-11-02 21:01:47 +0100

    inicjalizacja projektu

---
+++ src/main/java/pl/pawelozdoba/britenet/BritenetApplication.java
@@ -0,0 +1,12 @@
+package pl.pawelozdoba.britenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BritenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BritenetApplication.class, args);
+	}
+}
